e855a146497ad7ba771b7572f4fc04ed51898c72
Royal Paschall <royal.paschall@gmail.com>
2021-03-04 17:28:12 -0500

    2nd commit

--- back-end/routers/users.js
+++ back-end/routers/users.js
@@ -10,17 +10,20 @@ userRouter.get('/me', celebrate({
     email: Joi.string().required().email(),
   }),
 }), getCurrentUser);
+
 userRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
+
 userRouter.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required()
   }),
 }), updateAvatar);
+
 userRouter.get('/:_id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().length(24).required(),
